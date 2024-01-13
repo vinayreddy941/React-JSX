4b40ef5f185c483f14e57b94e1a596f46e2e4a0f
vinayreddy941 <vinayeee2001@gmail.com>
2024-01-13 11:12:52 +0530

    copy

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,11 @@
 function Message() {
-  return <h1>Hellmnbo World</h1>;
+  const name = "vsnay";
+
+  if (name == "vsnay") {
+    return <h1>Helmnbo World {name}</h1>;
+  } else {
+    return <h1>name is not there</h1>;
+  }
 }
 
 export default Message;
